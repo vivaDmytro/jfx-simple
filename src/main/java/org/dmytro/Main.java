011a8fb54a5a588c1035782b6c0ea78cb56e4d50
Dmitry Budnik <dmitry_budnik@i.ua>
2015-04-05 15:17:08 +0300

    application skeleton

---
+++ src/main/java/org/dmytro/Main.java
@@ -0,0 +1,24 @@
+package org.dmytro;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String fxmlFile = "/fxml/main.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
---
+++ src/main/java/org/dmytro/controller/MainController.java
@@ -0,0 +1,4 @@
+package org.dmytro.controller;
+
+public class MainController {
+}
